d27c7e4121448350bb5f8d85a22f931f52ace311
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2019-11-25 18:17:39 +0530

    Create bqndbvariables.tf

---
+++ bqndbvariables.tf
@@ -0,0 +1,13 @@
+variable "gcp_project_id" {} 
+variable "provider_region" {}
+variable "ip_name" {}
+variable "subnet_region" {} 
+variable "sql_machine_type" {}
+variable "vpc_name" {}
+variable "dataset_id" {}
+variable "bq_friendly_name" {}
+variable "bq_location" {}
+variable "bq_owner_email" {}
+variable "bucket_name" {}
+variable "bucket_location" {}
+variable "bucket_class" {}
