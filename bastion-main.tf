77488c7f2e632df530677f2ff3452813b5912773
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2019-12-20 15:23:49 +0530

    Create bastion-main.tf

---
+++ bastion-main.tf
@@ -0,0 +1,79 @@
+resource "google_compute_instance" "bastion" {
+  count = 1
+  name = "bastion"
+  machine_type = "n1-standard-1"
+  zone = "${var.gcp_zone}"
+  tags = ["bastion"]
+  boot_disk {
+    initialize_params {
+      image = "${var.gcp_amis}"
+    }
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.public.name}"
+    access_config {
+        # Ephemeral
+    }
+  }
+  metadata_startup_script = "${data.template_file.sysprep-bastion.rendered};"
+ metadata = {
+    sshKeys = "root:${file(var.bastion_key_path)}"
+  }
+}
+resource "google_compute_instance" "master1" {
+  count = 1
+  name = "master1"
+  machine_type = "n1-standard-2"
+  zone = "${var.gcp_zone}"
+  tags = ["master"]
+  boot_disk {
+    initialize_params {
+      image = "${var.gcp_amis}"
+    }
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.private.name}"
+  }
+   metadata_startup_script = "${data.template_file.sysprep-openshift.rendered};"
+  metadata = {
+    sshKeys = "root:${file(var.bastion_key_path)}"
+  }
+}
+resource "google_compute_instance" "infra1" {
+  count = 1
+  name = "infra1"
+  machine_type = "n1-standard-2"
+  zone = "${var.gcp_zone}"
+  tags = ["infra"]
+  boot_disk {
+    initialize_params {
+      image = "${var.gcp_amis}"
+    }
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.private.name}"
+  }
+  metadata_startup_script = "${data.template_file.sysprep-openshift.rendered};"
+  metadata = {
+    sshKeys = "root:${file(var.bastion_key_path)}"
+  }
+}
+resource "google_compute_instance" "worker1" {
+  count = 1
+  name = "worker1"
+  machine_type = "n1-standard-2"
+  zone = "${var.gcp_zone}"
+  tags = ["worker"]
+  boot_disk {
+    initialize_params {
+      image = "${var.gcp_amis}"
+    }
+  }
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.private.name}"
+  }
+  metadata_startup_script = "${data.template_file.sysprep-openshift.rendered};"
+  metadata = {
+    sshKeys = "root:${file(var.bastion_key_path)}"
+  }
+}
